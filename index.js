0bb1286eb0591cdcea371d432b4230558acb7110
Remi Galvez <remi.galvez@yahoo.com>
2015-11-10 21:26:22 -0500

    project structure

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(request, response) {
   response.render('pages/index');
