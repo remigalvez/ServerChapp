6c897ece985a4edb2d244c0a9af932a25687cac4
Remi Galvez <remi.galvez@yahoo.com>
2015-11-11 01:31:45 -0500

    code endpoint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
 	var user = {
 		name:"Remi Galvez",
 		gender:"Male"
 	}
-  response.send(user);
+  response.end(user);
 });
 
 app.listen(app.get('port'), function() {
