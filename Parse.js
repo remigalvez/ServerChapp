5de2a8268e72285c1e352fd04cb83a041e5b185b
Remi Galvez <remi.galvez@yahoo.com>
2015-11-12 22:16:00 -0500

    query app name from parse

--- Parse.js
+++ Parse.js
@@ -1,13 +1,65 @@
+var fs = require('fs');
+var Parse = require('parse/node').Parse;
+var AppID = require('./res/AppIDs');
+
+var idMap = AppID.map;
+
 const APP_ID = '1A5G2YPnZmvYoDII7wkZYoMPk0NKm3JKUAiXPsD2';
-const CLIENT_KEY = 'jFusc2WaHUGIDscXCjsIWiJQ00yv9i62jzsyIwjq';
 const JS_KEY = 'h7qocgTb5imqYv5Cle9nMOEzEGp2XICd70o78zxY';
-const API_KEY = '27ZYJChAXJHpbXHu9Bd6GWojgJXFzlsubrvxvwou';
 const MASTER_KEY = 'FzPvwcAIoF8ZaSn5k36vc44C5Erk1kLNPOvhbW5G';
 
-export {
-	APP_ID,
-	CLIENT_KEY,
-	JS_KEY,
-	API_KEY,
-	MASTER_KEY
+Parse.initialize(APP_ID, JS_KEY, MASTER_KEY);
+
+//var getUser = new Parse.Query(Parse.User);
+//getUser.get('yMqqXTXKsS', {
+//	success: function (user) {
+//		console.log(user.get('apps'));
+//	},
+//	error: function (e) {
+//		console.log(e);
+//	}
+//});
+
+//var getApps = new Parse.Query('App');
+//getApps.find({
+//	success: function (apps) {
+//		for (var i = 0; i < apps.length; i++) {
+//			console.log(apps[i].get('name'));
+//		}
+//	}
+//});
+
+
+var appTitle;
+exports.getTitle = function (appHandle, callback) {
+	appTitle = '';
+
+	var appId = idMap[appHandle];
+	var app = new Parse.Query('App');
+	app.get(appId, {
+		success: function (title) {
+			appTitle = title.get('name');
+		},
+		error: function (e) {
+			appTitle = 'Error';
+		}
+	}, callback(appTitle));
 }
+
+var welcomeMessage;
+exports.getWelcomeMessage = function (appHandle, callback) {
+	welcomeMessage = '';
+
+	var appId = idMap[appHandle];
+	var app = new Parse.Query('App');
+	app.get(appId, {
+		success: function (title) {
+			welcomeMessage = title.get('name');
+		},
+		error: function (e) {
+			welcomeMessage = 'Error';
+		}
+	}, callback(welcomeMessage));
+}
+
+
--- index.js
+++ index.js
@@ -2,16 +2,37 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
+var db = require('./parse.js');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send({name: 'Remi'});
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end({name: 'Remi'});
+});
+
+app.get('/:id/name', function (req, res) {
+    var appHandle = req.params.id;
+    //res.writeHead(200, {'Content-Type':'application/json'});
+    db.getTitle(appHandle, function (title) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end({'title': title});
+    });
+});
+
+app.get('/:id/welcome_message', function (req, res) {
+    var appHandle = req.params.id;
+    //res.writeHead(200, {'Content-Type':'application/json'});
+    db.getWelcomeMessage(appHandle, function (title) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end({'title': title});
+    });
 });
 
 app.get('/uber', function (req, res) {
 	var img = fs.readFileSync('./res/UberAppIcon.png');
-  res.writeHead(200, {'Content-Type': 'image/png' });
-  res.end(img, 'binary');
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    res.end(img, 'binary');
 });
 
 app.listen(app.get('port'), function() {
@@ -19,4 +40,29 @@ app.listen(app.get('port'), function() {
 });
 
 
-// https://myAppID:javascript-key=myJavaScriptKey@api.parse.com/1/classes/GameScore/Ed1nuqPvcm
+
+//var http = require('http');
+//
+//var bodyString = JSON.stringify({
+//    username: 'thefourtheye',
+//    password: '********'
+//});
+//
+//var headers = {
+//    'Content-Type': 'application/json',
+//    'Content-Length': bodyString.length
+//};
+//
+//var options = {
+//    host: 'localhost',
+//    path: '/users/1',
+//    port: 3000,
+//    method: 'PUT',
+//    headers: headers
+//};
+//
+//// callback is same as in the above seen example.
+//...
+//...
+
+//http.request(options, callback).write(bodyString);
